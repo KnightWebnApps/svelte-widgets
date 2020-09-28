86de58148c4dd168d1ae9da5019fcc288a43f20c
KnightWebnApps <45805254+KnightWebnApps@users.noreply.github.com>
2020-09-28 04:30:42 -0400

    Create Footer.svelte

---
+++ Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
+<footer>
+  <slot {year} ></slot> 
+</footer>
+
+<style>
+  footer {
+    text-align: center;
+    color: #33333365;
+  }
+</style>
