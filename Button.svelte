dcd30651b1295227659815bd379750371114da5e
KnightWebnApps <45805254+KnightWebnApps@users.noreply.github.com>
2020-09-28 04:13:23 -0400

    Create Button.svelte

---
+++ Button.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let onClick = () => {}
+  export let disabled = false
+
+</script>
+
+<button {disabled} on:click={onClick}>
+  <slot name="icon"></slot>
+  <slot>Submit</slot>
+</button>
